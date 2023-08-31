f7cef985742e243fe6d0a51487057fdf850460b7
Moises56 <moiavil56@gmail.com>
2023-08-31 00:45:33 -0600

    back

--- src/config.js
+++ src/config.js
@@ -1,23 +1,25 @@
 import { config } from "dotenv";
 config();
 
-// export const database = {
-//   connectionLimit: 10,
-//   host: process.env.DATABASE_HOST || "containers-us-west-179.railway.app",
-//   user: process.env.DATABASE_USER || "root",
-//   password: process.env.DATABASE_PASSWORD || "GYHjbuQtrvQkbitrS9gL",
-//   database: process.env.DATABASE_NAME || "railway",
-//   port: process.env.DATABASE_PORT || 7199,
-// };
-
+// db railway
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "localhost",
+  host: process.env.DATABASE_HOST || "containers-us-west-179.railway.app",
   user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "asd456",
-  database: process.env.DATABASE_NAME || "linksdb",
-  port: process.env.DATABASE_PORT || 3307,
+  password: process.env.DATABASE_PASSWORD || "GYHjbuQtrvQkbitrS9gL",
+  database: process.env.DATABASE_NAME || "railway",
+  port: process.env.DATABASE_PORT || 7199,
 };
+
+// db local
+// export const database = {
+//   connectionLimit: 10,
+//   host: process.env.DATABASE_HOST || "localhost",
+//   user: process.env.DATABASE_USER || "root",
+//   password: process.env.DATABASE_PASSWORD || "asd456",
+//   database: process.env.DATABASE_NAME || "linksdb",
+//   port: process.env.DATABASE_PORT || 3307,
+// };
 
 export const port = process.env.PORT || 3000;
 
